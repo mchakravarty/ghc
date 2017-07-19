a16f043bd68a9f328f638fd4c7308715c99622b7
Jared Weakly <jweakly@pdx.edu>
2017-07-18 17:35:34 -0700

    Small changes to address Ben's comments
    Signed-off-by: Jared Weakly <jweakly@pdx.edu>

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -1085,7 +1085,6 @@ def stats( name, way, stats_file ):
 # Check -t stats info
 
 def checkStats(name, way, stats_file, range_fields):
-
     full_name = name + '(' + way + ')'
 
     result = passed()
